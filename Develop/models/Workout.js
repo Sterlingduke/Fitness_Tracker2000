b01f9c2e59a4cb86015f6198eddede304d2fadd4
Sterling <sterlingcduke@gmail.com>
2021-02-24 23:00:34 -0600

    server.js

---
+++ Develop/models/Workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+
+  exercise:[
+    { 
+        type: {
+        type: String,
+        trim: true,
+        required:"Exercise Type" 
+        },
+
+        duration: {
+            type: Number,
+            trim: true,
+            required:"Exercise Time"
+        },
+
+        weight: {
+            type: Number
+           
+        },
+
+        reps: {
+            type: Number
+            
+        },
+
+        sets: {
+            type:Number
+            
+        },
+
+        distance: {
+            type: Number
+           
+        }
+    }
+
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+//  type: "resistance",
+// name: "Bicep Curl",
+// duration: 20,
+// weight: 100,
+// reps: 10,
+// sets: 4
